15bb71a56db425fe7e40450a70c619185d75d9aa
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-12-06 12:30:12 +0900

    add npm parcel and first fetch api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} Error code: ${res.status}`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      servings: recipe.servings,
+      image: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      publisher: recipe.publisher,
+      source: recipe.source_url,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    console.error(err);
+    // alert(err);
+  }
+};
+showRecipe();
