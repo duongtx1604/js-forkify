caf954903b99b6539e0703443a0bebda1bcdf779
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-12-06 15:31:25 +0900

    add events

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -604,9 +604,12 @@ const renderSpinner = function(parentEl) {
 const showRecipe = async function() {
     // 1) Load api
     try {
+        //get hash
+        const id = window.location.hash.slice(1);
+        if (!id) return;
         // spinner effect
         renderSpinner(recipeContainer);
-        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} Error code: ${res.status}`);
         let { recipe } = data.data;
@@ -715,7 +718,10 @@ const showRecipe = async function() {
     // alert(err);
     }
 };
-showRecipe();
+[
+    "hashchange",
+    "load"
+].forEach((ev)=>window.addEventListener(ev, showRecipe));
 
 },{"url:../img/icons.svg":"loVOp","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ"}],"loVOp":[function(require,module,exports) {
 module.exports = require("9bcc84ee5d265e38").getBundleURL("hWUTQ") + "icons.dfd7a6db.svg" + "?" + Date.now();
--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,16 @@ const renderSpinner = function (parentEl) {
 const showRecipe = async function () {
   // 1) Load api
   try {
+    //get hash
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // spinner effect
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -155,4 +160,4 @@ const showRecipe = async function () {
     // alert(err);
   }
 };
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
